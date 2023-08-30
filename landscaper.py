4ec2800e9e895700245c9a3f1e24e959c28767c5
Mark D. Lalich <mdlalich@gmail.com>
2023-08-29 19:01:34 -0700

    first comit

---
+++ landscaper.py
@@ -0,0 +1,17 @@
+#Starting a landscaping business
+
+#Using teeth cut grass once per day for $1 can do as many times
+
+user_input = input('Hello would you like to build a landscaping business, yes or no?')
+if user_input.lower() == 'yes':
+    landscaper_stats = {
+        '$$$': 0,
+        quit: False
+    }
+    print(f"Congratulaions on starting your business, you are starting from the bottom with ${landscaper_stats['$$$']} and can only cut grass with your teeth!")
+if user_input.lower() == 'no':
+    print('Thank you for the support have a nice life not winning')
+
+
+
+
